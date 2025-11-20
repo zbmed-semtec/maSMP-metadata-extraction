f8d9d6b6d4a933146fc05fe947f87f1a3c119231
rohitharavinder <rohitha0112@gmail.com>
2025-11-20 22:53:31 +0000

    feat: add core entities (RepositoryMetadata, Person, License, etc.)

---
+++ backend-migration/app/core/entities/__init__.py
@@ -0,0 +1,11 @@
+"""Core entities"""
+from app.core.entities.repository_metadata import (
+    RepositoryMetadata,
+    Person,
+    VersionControlSystem,
+    License,
+    ReferencePublication
+)
+
+__all__ = ["RepositoryMetadata", "Person", "VersionControlSystem", "License", "ReferencePublication"]
+
